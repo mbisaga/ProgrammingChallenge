e94b84fc3a22a5e3e62ecf8b676e7332cd6c8950
mbisaga <mbisaga@stedwards.edu>
2017-03-31 11:46:19 -0500

    Final Commit | Added up/down logic to SimulationImpl

--- ProgrammingChallenge/src/Simulation.java
+++ ProgrammingChallenge/src/Simulation.java
@@ -5,6 +5,7 @@ public interface Simulation {
 	//part of pre: Closest elevator answers call
 	//part of pre: Unless moving elevator is passing that floor
 	//part of pre: elevator cannot proceed above top floor or below ground floor
+	//post: returns true if successful, otherwise false
 	public boolean callElevatorRequest(int originFloor, int destinationFloor);
 	
 	//create a queue to keep track of requests
--- ProgrammingChallenge/src/SimulationImpl.java
+++ ProgrammingChallenge/src/SimulationImpl.java
@@ -8,12 +8,12 @@ public class SimulationImpl implements Simulation {
 	private int topFloor;
 	private int groundFloor = 1;
 	ArrayList<Elevator> elevators;
-	
+
 	public SimulationImpl(int numberOfElevators, int numberOfFloors) {
 		this.numberOfElevators = numberOfElevators;
 		this.numberOfFloors = numberOfFloors;
 		topFloor = numberOfFloors; 
-		
+
 		//initializing elevators
 		elevators = new ArrayList<Elevator>();
 		for (int i = 0; i < numberOfElevators; i++) {
@@ -21,13 +21,45 @@ public class SimulationImpl implements Simulation {
 			elevators.add(e1);	
 		}
 	}
-	
-	
+
+
 	public boolean callElevatorRequest(int originFloor, int destinationFloor) {
 		
+		Elevator e1 = findClosestElevator();
+		
+		if (originFloor>destinationFloor) { //going down! 
+			setIsGoingDown(e1);
+			//set destination floor
+		}
+		if (originFloor<destinationFloor) { //going up!{
+			setIsGoingUp(e1);
+			//set destination floor
+		}
+		else {
+			setIsStationary(e1);
+			//set destination floor
+		}
+	}
+
+	private Elevator findClosestElevator() {
+		//looks at elevators on each floor and returns most optimal one according to requirements
 	}
 	
-	private Elevator findClosestElevator() {
-		
+	private void setIsGoingDown(Elevator e1) {
+		e1.setisGoingDown(true);
+		e1.setisGoingUp(false);
+		e1.setisStationary(false);
+	}
+	
+	private void setIsGoingUp(Elevator e1) {
+		e1.setisGoingDown(false);
+		e1.setisGoingUp(true);
+		e1.setisStationary(false);
+	}
+	
+	private void setIsStationary(Elevator e1) {
+		e1.setisGoingDown(false);
+		e1.setisGoingUp(false);
+		e1.setisStationary(true);
 	}
 }

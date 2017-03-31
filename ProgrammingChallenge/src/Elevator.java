fdd17987f410634285f6642a85317f333ec12b1c
mbisaga <mbisaga@stedwards.edu>
2017-03-31 10:04:12 -0500

    15 minutes | Building interfaces and objects

--- ProgrammingChallenge/src/Elevator.java
+++ ProgrammingChallenge/src/Elevator.java
@@ -1,4 +1,14 @@
 
 public interface Elevator {
 
+	//Prints onto console what floor it is on as it is moving
+	//pre: floor>=1 && floor<=topFloor
+	public void sendFloorReport();
+	
+	public void sendDoorOpenReport();
+	
+	public void sendDoorClosedReport();
+	
+	public boolean isDoorOpen();
+	
 }
---
+++ ProgrammingChallenge/src/SimulationImpl.java
@@ -0,0 +1,9 @@
+
+public class SimulationImpl implements Simulation {
+
+	private int numberOfElevators;
+	private int numberOfFloors;
+	private int topFloor;
+	private int bottonFloor;
+	
+}
